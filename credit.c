86d749f50dc2136e24d7e6754eafb0062d16320f
Yahya Hassan <yah.hassan3@gmail.com>
2024-10-16 01:06:20 +0300

    added headers

--- credit.c
+++ credit.c
@@ -2,20 +2,18 @@
 #include <stdio.h>
 #include <math.h>
 
-long mor(long dig); //function for checking the digit digit*2 > 10 [cont. line 57]
-long lastd(long card); //function for taking the remainder [cont. line 55]
+long mor(long dig);    // function for checking the digit digit*2 > 10 [cont. line 57]
+long lastd(long card); // function for taking the remainder [cont. line 55]
 int main(void)
 {
-    long crd; //card number
-
+    long crd; // card number
 
     do
     {
         crd = get_long("Number: ");
-    }
-    while (crd <= 0);
+    } while (crd <= 0);
 
-    long digit1 = lastd(crd);                //extracting digits
+    long digit1 = lastd(crd); // extracting digits
     long digit2 = mor(lastd((crd / 10)));
     long digit3 = lastd((crd / 100));
     long digit4 = mor(lastd((crd / 1000)));
@@ -32,17 +30,17 @@ int main(void)
     long digit15 = lastd((crd / 100000000000000));
     long digit16 = mor(lastd((crd / 1000000000000000)));
 
-    int sum1 = digit2 + digit4 + digit6 + digit8 + digit10 + digit12 + digit14 + digit16;     //first summation (even indices)*2
+    int sum1 = digit2 + digit4 + digit6 + digit8 + digit10 + digit12 + digit14 + digit16; // first summation (even indices)*2
     int sum2 = sum1 + digit1 + digit3 + digit5 + digit7 + digit9 + digit11 + digit13 +
-               digit15;                                                                       //second summation (sum1 + odd indices)
+               digit15; // second summation (sum1 + odd indices)
 
-    if (sum2 % 10 == 0)   // checking cratiria
+    if (sum2 % 10 == 0) // checking cratiria
     {
         if (digit16 == 1 && (digit15 > 0 && digit15 < 6))
         {
             printf("MASTERCARD\n");
         }
-        else if (digit16 == 0 && digit15 == 3 && (digit14 == 8 || digit14 == 5))  // determine card type
+        else if (digit16 == 0 && digit15 == 3 && (digit14 == 8 || digit14 == 5)) // determine card type
         {
             printf("AMEX\n");
         }
@@ -61,11 +59,11 @@ int main(void)
     }
 }
 
-long lastd(long card)  //function for taking the remainder
+long lastd(long card) // function for taking the remainder
 {
     return card % 10;
 }
-long mor(long dig)  //function for checking the digit digit*2 > 10
+long mor(long dig) // function for checking the digit digit*2 > 10
 {
     if (dig == 5)
     {
@@ -97,5 +95,3 @@ long mor(long dig)  //function for checking the digit digit*2 > 10
         return dig * 2;
     }
 }
-
-
